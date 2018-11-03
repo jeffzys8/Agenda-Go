1bd6b64c661f2710b5ca5a55bf978aa85fceb059
yyhyplxyz <yangyh57@mail2.sysu.edu.cn>
2018-11-03 23:33:35 +0800

    upload scrawl

---
+++ 并发爬虫/scrawl/zhenai/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"scrawl/engine"
+	"scrawl/scheduler"
+	"scrawl/types"
+	"scrawl/zhenai/parser"
+)
+
+func main() {
+	e := engine.ConcurrentEngine{
+		Scheduler:   &scheduler.QueuedScheduler{},
+		WorkerCount: 100,
+	}
+	e.Run(types.Request{
+		Url:       "http://www.zhenai.com/zhenghun",
+		ParseFunc: parser.ParseCityList,
+	})
+}
